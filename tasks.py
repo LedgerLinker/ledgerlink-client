2feff75cb2c1ed10919c7403c1b3932cc34609d8
Russell McLoughlin <mcloughlin@amyris.com>
2023-07-28 11:58:08 -0700

    add task for building client distribution.

--- tasks.py
+++ tasks.py
@@ -11,3 +11,13 @@ def typecheck(c):
 def test(c):
     "Run nostests and generate coverage report."""
     c.run('python -m pytest --cov-report xml:cov.xml --cov', pty=True)
+
+
+@task
+def build_dist(c):
+    """Steps to build and package supergsl as library for pypi."""
+    c.run('pip install twine')
+    c.run('rm -rf build dist')
+    c.run('python setup.py sdist bdist_wheel')
+    c.run('twine check dist/*')
+    c.run('echo "Build complete, run \'twine upload dist/*\' to upload to pypi."')
